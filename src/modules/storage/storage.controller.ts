8c2c35117ba9e794287444c63d60518b32e09c1c
zgren <82068619@qq.com>
2020-04-28 15:10:54 +0800

    添加接口文档中文说明

--- src/modules/storage/storage.controller.ts
+++ src/modules/storage/storage.controller.ts
@@ -13,31 +13,37 @@ import { Storage } from '../../entity/storage.entity';
 import { StorageDto } from './storage.dto';
 import { PageResult } from '../page.common';
 import { StorageQuery } from '../storage/storage.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
 @Controller('storage')
 @ApiTags('对象存储数据操作')
 export class StorageController {
   constructor(private readonly storageService: StorageService) {}
+  @ApiOperation({ summary: '新增数据' })
   @Post()
   create(@Body() storageDto: StorageDto): Promise<Storage> {
     return this.storageService.create(storageDto);
   }
-
+  @ApiOperation({ summary: '分页数据查询' })
   @Get('page')
   findPage(@Query() storageQuery: StorageQuery): Promise<PageResult<Storage>> {
     return this.storageService.findPage(storageQuery);
   }
-
+  @ApiOperation({ summary: '根据id查询' })
   @Get(':id')
   findOne(@Param('id') id: string): Promise<Storage> {
     return this.storageService.findOne(id);
   }
 
+  @ApiOperation({ summary: '逻辑删除，实际是修改' })
   @Put(':id')
   delete(@Param('id') id: string) {
     return this.storageService.delete(id);
   }
 
+  @ApiOperation({
+    summary: '物理删除',
+    description: '物理删除,从数据库中删除数据',
+  })
   @Delete(':id')
   remove(@Param('id') id: string): Promise<void> {
     return this.storageService.remove(id);
